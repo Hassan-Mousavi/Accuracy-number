947c6d140e28fd99fca6a8fe635ad6be535af752
Hassan <hassanmousavi400@gmail.com>
2025-05-21 22:22:15 +0330

    exchages

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const num1 = document.querySelector(".num1");
 const num2 = document.querySelector(".num2");
 const result = document.querySelector(".result");
 const oparator = document.querySelector(".oparator");
+const plusIcon = document.querySelector(".plus");
+const minusBtn = document.querySelector(".minus");
 // btns part
-const trueBtn = document.querySelector(".true");
-const falseBtn = document.querySelector(".false");
+const nextBtn = document.querySelector(".next");
+const deleteBtn = document.querySelector(".delete");
 // seconds counter
 const secondCounter = document.querySelector(".time_question");
 // scores part
@@ -14,10 +16,13 @@ const score = document.querySelector(".scores");
 // scores part
 score.value = "0";
 // number part
-const number1 = (num1.value = Math.trunc(Math.random() * 45));
-const number2 = (num2.value = Math.trunc(Math.random() * 45));
-const totalResult = (result.value = Math.trunc(Math.random() * 45));
+const number1 = (num1.value = Math.trunc(Math.random() * 60));
+const number2 = (num2.value = Math.trunc(Math.random() * 60));
+// const totalResult = (result.value = Math.trunc(Math.random() * 45));
 
-if (totalResult === number1 + number2) {
-  score.value = 20;
-}
+nextBtn.addEventListener("click", function () {
+  plusIcon.classList.toggle("minus");
+  // minusBtn.classList.toggle("plus");
+  minusBtn.style.display = "block";
+  plusIcon.style.display = "none";
+});
